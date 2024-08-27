3f2a0ef9b5f86d5441187a9a55eedb490dc66833
Guibor_Camargo <guibor.camargo@urosario.edu.co>
2024-08-27 12:31:48 -0500

    Modelo equilibrio ver02

--- 2_3_api_proyeccion_economica/utils.R
+++ 2_3_api_proyeccion_economica/utils.R
@@ -22,3 +22,38 @@ renovo_base = c("#C4DCE2","#D2DAA6", "#F1F1F1")
 my_theme <- 
   theme_minimal()+
   theme(text = element_text(family = "serif"))
+
+
+# Funcion de saturación espacial
+# Crea dummys en fucion de rangos
+
+dist_saturation <- function(base, variable) {
+  # Extraer nombre de la variable
+  var_name = base %>% select({{variable}}) %>% names()
+  
+  # Aplicar la saturación
+  base <- base %>% 
+    arrange({{variable}}) %>% 
+    mutate(
+      test = {{variable}},
+      !!var_name := cut({{variable}}, breaks = c(seq(0,500,50),Inf),
+                        labels = 
+                          c(paste0(
+                            var_name,"_",
+                            letters[1:11],"_",
+                            seq(0,500,50),"_",
+                            c(seq(50,500,50),"o_mas"))))) %>% 
+    mutate(dummy = 1) %>% 
+    pivot_wider(names_from = {{variable}},
+                values_from = dummy,
+                values_fill = 0)
+  
+  return(base)
+  
+}
+
+
+# Imposición Funcional grado 2
+b1 = 2583
+b2 = -26
+(b1*-1)/(2*b2)
