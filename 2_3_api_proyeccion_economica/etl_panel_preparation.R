99a1d577b5a01b9b31f4dac403f92c0ea5993b94
Guibor AGATA <guibor.camargo@urosario.edu.co>
2024-08-29 01:37:06 -0500

    Saturaciones espaciales modelo de equilñibrio

--- 2_3_api_proyeccion_economica/etl_panel_preparation.R
+++ 2_3_api_proyeccion_economica/etl_panel_preparation.R
@@ -3,149 +3,109 @@ source("2_3_api_proyeccion_economica/utils.R")
 
 # 1. Cargar información ----
 
-## 1.1 Información Castastral ----
-a0_raw <- read.csv("..\\0_raw_data/5_bases_tabulares/Lotes_final.csv")
-a0_raw_2 <- read.csv("..\\0_raw_data/5_bases_tabulares/Lotes_distancia_ver2.csv", sep = ";") %>% 
-  select(LotCodigo,starts_with("SU"),lon = X,lat = Y)
-  
-
-a0_raw_2 %>% 
-  select(!starts_with("SU")) %>% 
-  head()
+## 1.1 Base casta ----
+a0_raw_catastro <- read.csv("..\\data/01_raw/mp01_matriz_por_lotes.csv")
+b0_raw_ccb <- read.csv("..\\data/01_raw/mc01_thesellas_1ha_ccb.csv", sep = ";")
+z0_var_dictionary <- read_sheet(link_diccionario)
 
 
 
+# 2. Curar información ccb
+b1_booked_ccb <- b0_raw_ccb %>% 
+  select(id00a_t01ha = GRID_ID, 
+         ec01_den_comercio = K_Comercio,
+         ec02_den_servicios = K_Servicio,
+         ec03_den_industria = K_Industria)
 
 
-b0_raw_2021 <- readRDS("..\\0_raw_data/5_bases_tabulares/20230620EM2021.rds")
 
 
-# 2. Cooked base lotes ----
-a1_cooked <- a0_raw %>% 
-  # Remplazar distancias
-  select(!starts_with("SU")) %>% 
-  merge(a0_raw_2, by = "LotCodigo", all.x = T) %>% 
-  
-  
-  ## 2.1. Edición de variables ----
-  mutate(
-    ### 2.1.1. Formato de distancias y coordenadas ----
-    across(starts_with("SU"),~as.numeric(str_replace(.,",","."))),
-    across(lon:lat,~as.numeric(str_replace(.,",","."))),
-     
-    ### 2.1.2. Cambio de alturas ----
-    Altura_Max = as.numeric(Altura_Max),
-    Area = as.numeric((str_replace(Area,",",".")))) %>%
-  
-  ## 2.2. Creación de variables ----
+# 3. Base final ----
+a1_cooked_catastro <- a0_raw_catastro %>% 
+  mutate(or02_cod_area_actividad = ifelse(
+    or02_cod_area_actividad ==" ","Sin AA",or02_cod_area_actividad)) %>% 
+  rename(in00a_longitude = X_y,
+         in00b_latitude = Y_y) %>%  # PENDIENTE: Corregir nombres y hay 2 longs 2 lats
+  #sample_n(1000) %>%
+  ## 3.1. Agregar información ----
+
+  ### 3.1.1. Información CCB -----
+  merge(b1_booked_ccb, by = "id00a_t01ha") %>% 
+  ### 3.1.2. Variaciones de precios
   rowwise() %>% 
   mutate(
     ### 2.2.1. Variación neta de pecios ----
     # 2 años
-    y02a_variacion_2a = VR_2023-VR_2021,
+    y02a_variacion_2a = y01d_valor_2023-y01c_valor_2021,
     # 5 años
-    y02b_variacion_5a =VR_2023-VR_2018,
+    y02b_variacion_5a = y01d_valor_2023-y01b_valor_2018,
     # 8 años
-    y02c_variacion_8a =VR_2023-VR_2015) %>% 
-  ungroup() %>% as.data.frame() %>% 
+    y02c_variacion_8a = y01d_valor_2023-y01a_valor_2015) %>% 
+  ungroup() %>%
   
-  ## 2.3. Selección de varibles ----
-  select(
-  ### 2.3.1. Variables de identificación ----
-    id01_lote = LotCodigo,
-    id02_manzana = ManzCodigo,
-    id03_localidad = Localidad,
-    id04_upl = UPL,
-    id05a_upz = Cod_UPZ,
-    id05b_upz_name = UPZ,
-    id06a_upz_em = Cod_UPZ_EM,
-    id06b_upz_em_name = UPZ_EM,
-  ### 2.3.2. Variables dependientes ----
-  # Años individuales
-  y01a_valor_2015 = VR_2015,
-  y01b_valor_2018 = VR_2018,
-  y01c_valor_2021 = VR_2021, 
-  y01d_valor_2023 = VR_2023,
-  # Variaciones netas
-  y02a_variacion_2a,
-  y02b_variacion_5a,
-  y02c_variacion_8a,
+  ## 3.2. Seleccionar y reodenar variables ----
+  select(all_of(z0_var_dictionary$variabe_name[z0_var_dictionary$variabe_name %in% names(.[])])) %>% 
   
-  ### 2.3.3. Independientes (in: Intrínsecas) ----
-  in01_altura = Altura_Max,
-  in02_area = Area,
-  in03a_uso_residencial = RESIDENCIAL, # Modelado
-  in03b_uso_comercial = COMERCIAL, # Modelado
-  in03c_uso_industrial = INDUSTRIAL,
-  in03d_uso_dotacional = DOTACIONAL,
-  in03e_uso_logistico = LOGISTICO,
-  in03f_uso_parqueadero = PARQUEADERO,
-  #in03g_uso_libre = LIBRE, # Es siempre CERO, revisar
-  in03h_uso_otro = OTRO,
+  ## 3.3. Eliminar ouliers ----
+  quantilic_filter(y01d_valor_2023) %>% 
+  quantilic_filter(y01c_valor_2021) %>% 
+  quantilic_filter(y01b_valor_2018) %>% 
+  quantilic_filter(y01a_valor_2015) %>% 
+  quantilic_filter(in04_m2_construidos) %>% 
   
-  ### 2.3.4. Independientes (hedónicas) ----
-  # Equipamietnos (eq)
-  eq01_educacion = SU_Eq_Edu1, # OJO !!! Falta Saturación por tipo
-  eq02_salud = SU_Eq_Salu1,
-  eq03_cultura = SU_Eq_Cul1,
-  eq04_recreacion = SU_Eq_Rec1,
-  # Espacio público (ep)
-  ep01_parques = SU_EP_Parq1, # OJO !!! falta saturar por tamaño             
-  ep02_plazas = SU_EP_Plaz1,
+  ## 3.4. Coercion Paila de formatos  (ESTO NO TIENE POR QUE PASAR )
+  mutate(across(c(28:40,43,45:47), ~as.numeric(str_replace(.,",",".")))) %>%
   
-  # Transporte (tr)
-  tr01_transmilenio = SU_Tr_TM1,
-  tr02_sitp = SU_Tr_SITP1,
+  # (*) REPORTE DE COMPLETITUD  
+  # a1_cooked_catastro %>% summarise(across(everything(),~mean(!is.na(.)))) %>% 
+  #   reshape2::melt() %>% 
+  #   googlesheets4::write_sheet(
+  #     ss = link_diccionario,
+  #     sheet = "completitud")
+  select(-or04_altura_max,-or03_area_actividad) %>% # (Eliminada por completitud) 
+  ## 3.4. Eliminar observaciones en missing ----
+  filter(if_all(1:ncol(.[]),~!is.na(.))) %>% 
   
-  # Ordenamiento (or)
-  or01_tratamietno = Tratamient,
-  or02_area_actividad = Area_Activida,
-  or03_altura_max =Altura_Max) %>% 
-  
-  ## 2.4. Eliminación de NA ----
-  filter(if_all(y01a_valor_2015:or03_altura_max,~!is.na(.))) %>% 
-  ## 2.5. Remoción de outliers ----
-  filter(
-    between(y01d_valor_2023, 
-            quantile(y01d_valor_2023,probs = .025),
-            quantile(y01d_valor_2023,probs = .975)) &
-      between(y01c_valor_2021, 
-              quantile(y01c_valor_2021,probs = .025),
-              quantile(y01c_valor_2021,probs = .975)) &
-      between(y01b_valor_2018, 
-              quantile(y01b_valor_2018,probs = .025),
-              quantile(y01b_valor_2018,probs = .975)) &
-      between(y01a_valor_2015, 
-              quantile(y01a_valor_2015,probs = .025),
-              quantile(y01a_valor_2015,probs = .975)) &
-      # por área
-      between(in02_area, 
-              quantile(in02_area,probs = .025),
-              quantile(in02_area,probs = .975))) %>% 
-  ## 2.6. Ediciones finales complementarias ----
+  ## 3.5. Coerción regular
   mutate(
     ### 2.6.1. Cambiar a factor o numeric ----
     across(where(is.character),as.factor),
-    across(where(is.integer),as.numeric),
-    ### 2.6.2. Áreas construidas por %
-    area_tot = rowSums(select(.,in03a_uso_residencial:in03h_uso_otro), na.rm = T),
-    across(in03a_uso_residencial:in03h_uso_otro,~./area_tot)) %>% 
-  select(-area_tot)
+    across(where(is.integer),as.numeric))
+
+  
+
 
 ## 2.7. Exportar la base ----
-saveRDS(a1_cooked, "..\\0_raw_data/2_rds_geo_data/m01_cooked_lotes_enrriquecidos.RDS")
+saveRDS(a1_cooked_catastro, "..\\data/02_cooked/m01_cooked_lotes_enrriquecidos.RDS")
   
 
 
 # 3. Tasty Base lotes ----
-a2_tasty <- a1_cooked %>% 
-  mutate(
-    # Loglinearización de precios
-    across(y01a_valor_2015:y02c_variacion_8a,~log(.+1)),
-    # Estadarización de variaciones e independientes
-    across(y02a_variacion_2a:tr02_sitp,~scale(.))) %>% 
-  # Re-eliminar todos los NA
-  filter(if_all(y01a_valor_2015:or03_altura_max,~!is.na(.))) 
+a2_tasty_catastro <- a1_cooked_catastro %>% 
+  ## Pre-Saturaciones
+  select(
+    # Remover generales sin sentido
+    -eq01a_educacion_general,
+    # Remover saturados sin sentido
+    -eq05b_seguridad_cais, -eq05c_seguridad_estaciones) %>% 
+  ## Saturaciones
+  dist_saturation(eq01b_educacion_colegios) #%>% 
+  dist_saturation(eq01c_educacion_universidades) %>% 
+  dist_saturation(eq02a_salud_general) %>% 
+  dist_saturation(eq05a_seguridad_general) %>% 
+  dist_saturation(ep01a_parques_general) %>% 
+  dist_saturation(ep02_plazas) %>% 
+  dist_saturation(tr01_transmilenio) %>% 
+  dist_saturation(tr02_sitp)
+
+
+  
+  names(a2_tasty_catastro)[28:38]
+
+
+names(a1_cooked_catastro)
+
+
 saveRDS(a2_tasty, "..\\0_raw_data/2_rds_geo_data/m01_tasty_lotes_enrriquecidos.RDS")
 
 
--- 2_3_api_proyeccion_economica/utils.R
+++ 2_3_api_proyeccion_economica/utils.R
@@ -1,3 +1,4 @@
+# 0. Librerias ----
 library(dplyr)
 library(tidyr)
 library(stringr)
@@ -10,7 +11,100 @@ library(googlesheets4)
 options(scipen = 20)
 
 
-## base fake
+
+# 1. Funciones ----
+
+## 1.1. Saturación de distancias ----
+### Crea dummies a partir de distancias 
+dist_saturation <- function(base, variable) {
+  # Extraer nombre de la variable
+  var_name = base %>% select({{variable}}) %>% names()
+  
+  # Aplicar la saturación
+  base <- base %>% 
+    arrange({{variable}}) %>% 
+    mutate(temporal_id = row_number()) %>% 
+    mutate( 
+      #ref = {{variable}},
+      {{variable}} := cut({{variable}}, breaks = c(seq(0,500,50),Inf),
+                        labels = 
+                          c(paste0(
+                            var_name,"_",
+                            letters[1:11],"_",
+                            seq(0,500,50),"_",
+                            c(seq(50,500,50),"o_mas"))),right = FALSE)) %>% 
+    mutate(dummy = 1) %>%
+    pivot_wider(
+      id_cols = everything(),
+      names_from = {{variable}},
+      values_from = dummy,
+      values_fill = 0) %>% 
+    select(-temporal_id)
+
+  return(base)
+  
+}
+
+
+pr <- data.frame(
+  y = "hola",
+  id = 1:100,
+  x = c(0,sample(0:10000,99)))
+
+pr <- pr %>% dist_saturation(x)
+
+pr <- pr %>% mutate(dummy = 1) %>%
+  pivot_wider(
+    id_cols = id,
+    names_from = x,
+    values_from = dummy)
+
+
+## 1.2 Filtro cuantilico ----
+### Filtra un % de la base teniendo en cuenta el valor cuantilico determinado
+### por ambas colas (i.e. 5% elimina el 2.5% inferior y 2.5% superior)
+
+
+quantilic_filter <- function(base, variable, expansion = 1.5) {
+  
+  var = base %>% pull({{variable}})
+  
+  lower_limit <- quantile(var,probs = .25, na.rm = T)-(IQR(var, na.rm = T)*expansion)
+  upper_limit <- quantile(var,probs = .75, na.rm = T)+(IQR(var, na.rm = T)*expansion)
+  
+  base <- base %>%
+    filter(
+      between({{variable}},
+              lower_limit,
+              upper_limit))
+  return(base)
+}
+
+
+
+
+# Links
+
+## Diccionario de datos
+link_diccionario <- "https://docs.google.com/spreadsheets/d/1QTTEdmje0WHwJcFuhGlq5sujp-Y2ABH5mKYk5XO-XeE/edit?usp=sharing"
+
+
+
+1-0.5
+
+# Imposición Funcional grado 2
+b1 = 2583
+b2 = -26
+(b1*-1)/(2*b2)
+
+
+
+
+
+
+
+
+# Funciones de diseño
 
 ### Paleta 1
 renovo = c("#a7e85d","#013334","#e36477","#62b7b2","#d1ce84")
@@ -24,35 +118,7 @@ my_theme <-
   theme(text = element_text(family = "serif"))
 
 
-# Funcion de saturación espacial
-# Crea dummys en fucion de rangos
-
-dist_saturation <- function(base, variable) {
-  # Extraer nombre de la variable
-  var_name = base %>% select({{variable}}) %>% names()
-  
-  # Aplicar la saturación
-  base <- base %>% 
-    arrange({{variable}}) %>% 
-    mutate( 
-      cuts := cut({{variable}}, breaks = c(seq(0,500,50),Inf),
-                        labels = 
-                          c(paste0(
-                            var_name,"_",
-                            letters[1:11],"_",
-                            seq(0,500,50),"_",
-                            c(seq(50,500,50),"o_mas"))))) %>% 
-    mutate(dummy = 1) %>% 
-    pivot_wider(names_from = cuts,
-                values_from = dummy,
-                values_fill = 0)
-  
-  return(base)
-  
-}
 
 
-# Imposición Funcional grado 2
-b1 = 2583
-b2 = -26
-(b1*-1)/(2*b2)
+
+
